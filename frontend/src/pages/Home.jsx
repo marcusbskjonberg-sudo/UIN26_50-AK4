0f1e4f84790f4821855e15df47c748788816430f
marcusbs-sudo <marcus.b.skjonberg@hiof.no>
2026-04-22 02:37:02 +0200

    Updated homepage and css
    Added useeffect to fetch info about the courses arbeidskrav from sanity.
    Added a section to map the arbeidskrav to and some css.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Marcus from '../components/Marcus.jsx'
+import { client } from '../helpers/sanityClient.js'
 
 export default function Home(){
     
     const [arbeidskrav, setArbeidskrav] = useState(null)
+
+    useEffect(() => {
+        async function getArbeidskrav() {
+            const tempArbeidskrav = await client.fetch("*[_type == 'arbeidskrav'] | order(_createdAt asc) {navn, beskrivelse, _id}")
+            setArbeidskrav(tempArbeidskrav)
+        }
+        getArbeidskrav()
+    }, [])
     
-    
+    console.log(arbeidskrav)
+
     return(<>
 
         <header>
@@ -14,6 +24,14 @@ export default function Home(){
         
         <main>
             <Marcus/>
+            <section id='arbeidskrav-section'>
+                <h2>Arbeidskrav</h2>
+                {arbeidskrav?.map((ak) => 
+                    <article key={ak._id} className='arbeidskrav-article'>
+                        <h3>{ak.navn}</h3>
+                        <p>{ak.beskrivelse}</p>
+                    </article>)}
+            </section>
         </main>
 
         </>)
