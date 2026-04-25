b4eec2cf3a6a551f34c1471d3de0c775e0e856b2
marcusbs-sudo <marcus.b.skjonberg@hiof.no>
2026-04-25 04:21:28 +0200

    Added comments
    Added comments to the code to explain different stuff and made some small adjustments

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,8 +4,11 @@ import { client } from '../helpers/sanityClient.js'
 
 export default function Home(){
     
+    //Holder på arbeidskravene når de hentes fra sanity, har en default verdi av null før de hentes
     const [arbeidskrav, setArbeidskrav] = useState(null)
 
+
+    //Henter arbeidskravene fra sanity når siden lastes med en tom dependency
     useEffect(() => {
         async function getArbeidskrav() {
             const tempArbeidskrav = await client.fetch("*[_type == 'arbeidskrav'] | order(_createdAt asc) {navn, beskrivelse, _id}")
@@ -13,8 +16,8 @@ export default function Home(){
         }
         getArbeidskrav()
     }, [])
+
     
-    console.log(arbeidskrav)
 
     return(<>
 
@@ -23,12 +26,16 @@ export default function Home(){
         </header>
         
         <main>
+            {/* Gruppemedlemmenes profil-komponenter legges her (jeg er eneste medlem i gruppen) */}
             <section id='profile-section'>
                 <Marcus/>
             </section>
+            {/* Arbeidskravene mappes ut i denne sectionen HVIS de eksisterer i useStaten "arbeidskrav" */}
             <section id='arbeidskrav-section'>
                 <h2>Arbeidskrav</h2>
-                {arbeidskrav?.map((ak) => 
+                {arbeidskrav?.map((ak) =>
+                    //Bruker id'en sanity har gitt hvert enkelt arbeidskrav som key i .map
+                    //kunne laget et eget komponent å bruke for .map men valgte å gjøre det på den enkle måten med å lage strukturen for .map her
                     <article key={ak._id} className='arbeidskrav-article'>
                         <h3>{ak.navn}</h3>
                         <p>{ak.beskrivelse}</p>
